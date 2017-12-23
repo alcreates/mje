93c1d0f758b86a17093636352c3f66421c3314f9
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2017-12-23 13:59:14 -0500

    about us

--- mjeAdvisors/src/app/app.module.ts
+++ mjeAdvisors/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
