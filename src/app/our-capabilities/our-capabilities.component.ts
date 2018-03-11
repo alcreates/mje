f9a5d5a03ffba88f9c9354e40340ca129c88b80f
Al Martinez <al@sitparker.com>
2018-03-11 17:04:42 -0400

    restructor for heroku

---
+++ src/app/our-capabilities/our-capabilities.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-our-capabilities',
+  templateUrl: './our-capabilities.component.html',
+  styleUrls: ['./our-capabilities.component.css']
+})
+export class OurCapabilitiesComponent implements OnInit {
+  isFlipped=false;
+  isFlippedTwo=false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
